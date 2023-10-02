fa52886e5a808adb07530ee895767ba6bc21a86b
MARTIN LEDEZMA <martynledezma@gmail.com>
2023-10-02 20:49:46 -0300

    Actualizar Clase1

---
+++ Martin Ledezma/Laboratorio2/Clase1/main.py
@@ -0,0 +1,19 @@
+
+print("Ejercicio 1: Iterar un rango de 0 a 10 e imprimir n° divisibles por 3")
+
+for i in range(11):
+    if i % 3 == 0:
+        print(i)
+
+# ejercicio 2: crear un rango de  2 a 6 e imprimirlos
+
+print("Ejercicio2: Rango con valores de inicio 2 y fin 6")
+rango= range(2, 7)
+for i in rango:
+    print(i)
+
+# ejercicio 3: crear un rango de 3 a 10 pero con incremento de 2 en 2
+print("Ejercicio 3: Rango de 3 a 10 con incremento de 2 en 2")
+for i in range(3, 11, 2):
+    print(i)
+

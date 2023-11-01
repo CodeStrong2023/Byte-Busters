4dc84b41bfd51a4a0845e92cb9129a32b1de8e06
Andres Luna <65298144+Luna201@users.noreply.github.com>
2023-11-01 19:51:30 -0300

    Add files via upload

---
+++ Andres Luna/Programacion 2/Clase5/ClaseScanner09.java
@@ -0,0 +1,12 @@
+/*
+Ejercicio 9: Pedir el dia, mes y año de una fecha e indicar si la fecha es correcta. 
+Suponiendo que todos los meses son de 30 dias
+ */
+package Ejercicio09;
+import java.util.Scanner;
+public class ClaseScanner09 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Digite el dia: ");
+    }
+}

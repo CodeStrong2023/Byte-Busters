745257ba16e028a80f6e0251137f707c8126e382
Andres Luna <65298144+Luna201@users.noreply.github.com>
2023-11-01 23:56:43 -0300

    Add files via upload

---
+++ Andres Luna/JavaScript/Leccion2/02-01-TiposDeDatos.js
@@ -0,0 +1,16 @@
+// Tipos de datos en JavaScript
+/*
+La sintaxis en los comentarios es similar a la de java
+*/
+var nombre = "Andres";  //tipo Str
+console.log(nombre); 
+
+var numero = 3000;  //tipon numerico
+console.log(numero);
+
+var objeto ={
+    nombre : "Andres",
+    apellido : "Luna",
+    telefono : "2604123456"
+}
+console.log(objeto);

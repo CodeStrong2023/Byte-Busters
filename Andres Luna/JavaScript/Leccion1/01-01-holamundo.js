8c4edacc46083bc13fce6455f225b06f1193115a
Andres Luna <65298144+Luna201@users.noreply.github.com>
2023-11-01 23:36:42 -0300

    Add files via upload

---
+++ Andres Luna/JavaScript/Leccion1/01-01-holamundo.js
@@ -0,0 +1,4 @@
+var nombre = "Ariel";
+nombre = "Hola mundo desde JavaScript";
+
+console.log(nombre);
---
+++ Andres Luna/JavaScript/Leccion1/01-holamundo.js
@@ -0,0 +1,7 @@
+var nombre = "Ariel";
+nombre = "Andres";
+var apellido = "Luna";
+console.log(nombre);
+console.log(apellido);
+
+console.log(nombre + apellido);

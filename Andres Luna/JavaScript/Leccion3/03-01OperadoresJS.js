73fd57c0a2f65be5cde27b12d628d9a8ec67dac8
Andres Luna <65298144+Luna201@users.noreply.github.com>
2023-11-02 20:47:50 -0300

    Add files via upload

---
+++ Andres Luna/JavaScript/Leccion3/03-01OperadoresJS.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: para encontrar números pares impares
+
+let parImpar= 10;
+if(parImpar % 2 ==0){
+    console.log("Es un número PAR")
+}
+else{
+    console.log("Es un número IMPAR")
+}
---
+++ Andres Luna/JavaScript/Leccion3/03-02OperadoresJS.js
@@ -0,0 +1,7 @@
+//Ejercicio 2: es mayor de edad
+
+let edad = 14, adulto = 18
+if(edad >= adulto){
+    console.log("Usted es una persona adulta");
+}
+else{console.log("Usted es una persona menor de edad");}

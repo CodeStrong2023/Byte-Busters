64f2c6acd9c87998b88790b5389ed71c142a343b
Andres Luna <65298144+Luna201@users.noreply.github.com>
2023-11-06 21:12:29 -0300

    Add files via upload

---
+++ Andres Luna/Laboratorio 2/Clase5/FuncionesDePython.py
@@ -0,0 +1,8 @@
+#Comenzamos con Funciones
+#Definimos una función
+# mi_funcion()      No se puede llamar antes de definir una función
+def mi_funcion():   #snake_case o camelCase
+    print("Saludos a todos los alumnos de la Tecnicatura")
+
+mi_funcion()    #Llamamos la función
+mi_funcion()

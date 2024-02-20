edc4eeb4fa5343e7a2a5ad59c98f898bc424aae3
Andres Luna <65298144+Luna201@users.noreply.github.com>
2024-02-20 16:54:32 -0300

    Update 02-01-TiposDeDatos.js

--- Andres Luna/JavaScript/Leccion2/02-01-TiposDeDatos.js
+++ Andres Luna/JavaScript/Leccion2/02-01-TiposDeDatos.js
@@ -5,7 +5,7 @@ La sintaxis en los comentarios es similar a la de java
 var nombre = "Andres";  //tipo Str
 console.log(nombre); 
 
-var numero = 3000;  //tipon numerico
+var numero = 3000;  //tipo numerico
 console.log(numero);
 
 var objeto ={
